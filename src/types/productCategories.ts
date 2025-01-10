f4e12154b4db9eee1640c6985d8be9c8e3a72497
m-istighfar <istighfar.amal@gmail.com>
2025-01-10 19:55:27 +0700

    feat: setup api call product categories

---
+++ src/types/productCategories.ts
@@ -0,0 +1,6 @@
+export interface ProductCategory {
+    product_category_id: string;
+    product_category_name: string;
+    product_category_desc: string;
+    product_category_created_date: string;
+}
